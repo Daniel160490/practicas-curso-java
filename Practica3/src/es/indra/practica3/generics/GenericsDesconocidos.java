7fea1e1b038945d8590bd9691a2d02cf89352391
Daniel160490 <danigd71@gmail.com>
2018-11-07 12:44:30 +0100

    Uso de generics desconocidos

---
+++ Practica3/src/es/indra/practica3/generics/GenericsDesconocidos.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package es.indra.practica3.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author P.era-1
+ *
+ */
+public class GenericsDesconocidos {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		List<Integer> listado = new ArrayList();
+		listado.add(23);
+		listado.add(45);
+		
+		
+		List<? extends Number> listadoDesconocido = new ArrayList();
+		listadoDesconocido.add(new Integer(34), null);
+	}
+
+}
